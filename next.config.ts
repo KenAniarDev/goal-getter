a58442357901c4e810a78b17e7c348b59d38728c
Nestor Kenneth Aniar <nkraniar@palawanpawnshop.com>
2025-11-26 11:52:22 +0800

    added next pwa

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,25 @@
 import type { NextConfig } from "next";
+import withPWAInit from "@ducanh2912/next-pwa";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const prod = process.env.NODE_ENV === "production";
 
-export default nextConfig;
+const withPWA = withPWAInit({
+  dest: "public",
+  disable: !prod,
+  register: true,
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  fallbacks: {
+    document: "/offline",
+  },
+  workboxOptions: {
+    disableDevLogs: true,
+    skipWaiting: true, // Moved inside workboxOptions
+    clientsClaim: true,
+  },
+});
+
+const nextConfig: NextConfig = {};
+
+export default withPWA(nextConfig);
